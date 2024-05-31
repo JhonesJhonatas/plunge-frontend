6aee7308ddf7b5aa2662bee150af9a8dcaf7fe5b
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-05-31 20:54:14 -0300

    feat: improvements on login page

--- app/(pages)/(public)/login/page.tsx
+++ app/(pages)/(public)/login/page.tsx
@@ -1,39 +1,48 @@
 import Image from 'next/image'
 
 import plungeHorizontalLogo from '@/app/assets/plunge-horizontal-logo.svg'
-import applicationScreen from '@/app/assets/application-screen.png'
+import applicationMockup from '@/app/assets/application-mockup.svg'
+
 import { Button, Input } from '@/app/components'
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
+import { BsLinkedin } from 'react-icons/bs'
 
 export default function Login() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       <div className="flex w-3/5 h-3/4 bg-gradient-to-r from-slate-800 to-slate-900 text-slate-50 rounded gap-8 shadow-2xl p-8 border-2 border-slate-800">
-        <section className="w-1/2 flex flex-col gap-8 justify-between">
-          <Image src={plungeHorizontalLogo} alt="" className="max-w-44" />
-          <div className="w-full flex flex-col gap-8 items-center">
-            <div className="flex flex-col gap-2 items-center">
-              <span className="text-3xl font-bold">LogIn na Plunge</span>
+        <section className="w-1/2 flex flex-col gap-36 justify-end">
+          <div className="w-3/4 flex flex-col gap-4 items-center mx-auto">
+            <div className="flex flex-col gap-4 items-center">
+              <div className="flex items-start gap-2">
+                <span className="text-3xl font-bold">LogIn na </span>
+                <Image src={plungeHorizontalLogo} alt="" className="max-w-36" />
+              </div>
               <span className="text-zinc-300">
                 Fique por dentro de tudo que envolve a Campus Party
               </span>
             </div>
             <div className="w-full flex items-center gap-2 justify-center">
-              <div>
+              <div className="flex items-center gap-2 bg-slate-700 px-4 py-2 rounded cursor-pointer hover:bg-slate-600 transition-all border-2 border-slate-600">
+                <FcGoogle />
                 <span>Google</span>
               </div>
-              <div>
+              <div className="flex items-center gap-2 bg-slate-700 px-4 py-2 rounded cursor-pointer hover:bg-slate-600 transition-all border-2 border-slate-600">
+                <FaGithub />
                 <span>Github</span>
               </div>
-              <div>
-                <span>Linkedin</span>
+              <div className="flex items-center gap-2 bg-slate-700 px-4 py-2 rounded cursor-pointer hover:bg-slate-600 transition-all border-2 border-slate-600">
+                <BsLinkedin />
+                <span>Google</span>
               </div>
             </div>
-            <div className=" flex gap-8 w-full justify-center items-center">
-              <hr className="w-1/6 border-zinc-600" />
+            <div className="w-full flex gap-8 justify-center items-center">
+              <hr className="w-3/12 border-zinc-600" />
               <span className="text-zinc-300">Ou utilize seu email</span>
-              <hr className="w-1/6 border-zinc-600" />
+              <hr className="w-3/12 border-zinc-600" />
             </div>
-            <form className="flex flex-col gap-4 w-3/5">
+            <form className="flex flex-col gap-4 w-full">
               <Input />
               <Input />
               <div className="w-full flex items-center justify-between">
@@ -65,7 +74,7 @@ export default function Login() {
         </section>
         <section className="w-1/2 overflow-hidden rounded">
           <Image
-            src={applicationScreen}
+            src={applicationMockup}
             alt=""
             className="object-cover h-full"
           />
